30588800acba42f1cc74b3fa6859d9bf6ce1f4bc
Sergey Agaka <sergeyagaka@gmail.com>
2023-01-29 01:36:01 +0300

    add IsAdminOrAnyoneCanCreate permission

--- server/equip_app/permissions.py
+++ server/equip_app/permissions.py
@@ -4,4 +4,4 @@ class IsAdminOrAnyoneCanCreate(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == 'POST':
             return True
-        return bool(request.user and request.user.is_staff)
+        return request.user and request.user.is_superuser
