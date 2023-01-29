bb1b4fcf04e04884f0bb1654fa32de85cc7514fb
Sergey Agaka <sergeyagaka@gmail.com>
2023-01-29 19:31:52 +0300

    init frontend app

--- server/equipsys/settings.py
+++ server/equipsys/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'equip_app'
+    'equip_app',
+    'frontend'
 ]
 
 MIDDLEWARE = [
--- server/equipsys/urls.py
+++ server/equipsys/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('rest_framework.urls')),
     path('api/app/', include('equip_app.urls'))
+    path('api/frontend/', include('frontend.urls'))
 ]
