35e0449e09536b4d569582f541cee43402447c5c
Sergey Agaka <sergeyagaka@gmail.com>
2023-02-12 04:37:01 +0300

    add delete org

--- server/frontend/views.py
+++ server/frontend/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import update_session_auth_hash
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import redirect
@@ -19,7 +18,8 @@ def sign_in_action(request):
         username = request.POST['username']
         password = request.POST['password']
 
-        response = requests.post('http://127.0.0.1:8000/api/app/login_user/', {'username': username, 'password': password})
+        response = requests.post('http://127.0.0.1:8000/api/app/login_user/',
+                                 {'username': username, 'password': password})
         if response.status_code == 200:
             resp = redirect('/main')
             response_cookies = response.cookies
@@ -67,17 +67,20 @@ def home(request):
 
         if request.user.is_superuser:
             users_data = (requests.get("http://127.0.0.1:8000/api/app/users/", cookies=cookies_now)).json()
-            organizations_data = (requests.get("http://127.0.0.1:8000/api/app/organizations/", cookies=cookies_now)).json()
+            organizations_data = (requests.get("http://127.0.0.1:8000/api/app/organizations/",
+                                               cookies=cookies_now)).json()
             current_data['all_users'] = users_data
             current_data['all_orgs'] = organizations_data
             return render(request, 'frontend/home-admin.html', context=current_data)
 
         #информация об организациях пользователя
-        current_organizations_data = (requests.get("http://127.0.0.1:8000/api/app/current_organizations/", cookies=cookies_now)).json()
+        current_organizations_data = (requests.get("http://127.0.0.1:8000/api/app/current_organizations/",
+                                                   cookies=cookies_now)).json()
         current_data['current_organizations'] = current_organizations_data
         #получаем оборудование для всех организаций текущего пользователя
         for org in current_data['current_organizations']:
-            current_organizations_equipment = (requests.get("http://127.0.0.1:8000/api/app/equip_of_org/?org_id="+str(org['id']), cookies=cookies_now)).json()
+            current_organizations_equipment = (requests.get("http://127.0.0.1:8000/api/app/equip_of_org/?org_id="+str(org['id']),
+                                                            cookies=cookies_now)).json()
             org['equipments'] = current_organizations_equipment
         # print('Гдееее тыыы')
         # print(my_data)
@@ -96,7 +99,8 @@ def delete_current_user(request):
     id_current_user = get_current_user['id']
     headers = {}
     headers['X-CSRFToken'] = cookies_now['csrftoken']
-    response = requests.delete("http://127.0.0.1:8000/api/app/users/"+str(id_current_user), cookies=cookies_now, headers=headers)
+    response = requests.delete("http://127.0.0.1:8000/api/app/users/"+str(id_current_user),
+                               cookies=cookies_now, headers=headers)
     print(response.status_code, response.content)
     return redirect('/')
 
@@ -105,7 +109,8 @@ def delete_user_id(request, user_id):
     cookies_now = {'csrftoken': request.COOKIES.get('csrftoken'), 'sessionid': request.COOKIES.get('sessionid')}
     headers = {}
     headers['X-CSRFToken'] = cookies_now['csrftoken']
-    response = requests.delete("http://127.0.0.1:8000/api/app/users/"+str(user_id), cookies=cookies_now, headers=headers)
+    response = requests.delete("http://127.0.0.1:8000/api/app/users/"+str(user_id), cookies=cookies_now,
+                               headers=headers)
     print(response.status_code, response.content)
     return redirect('/')
 
@@ -114,7 +119,8 @@ def del_equip(request, equip_id):
     id_current_equipment = equip_id
     headers = {}
     headers['X-CSRFToken'] = cookies_now['csrftoken']
-    response = requests.delete("http://127.0.0.1:8000/api/app/equipments/" + str(id_current_equipment), cookies=cookies_now, headers=headers)
+    response = requests.delete("http://127.0.0.1:8000/api/app/equipments/" + str(id_current_equipment),
+                               cookies=cookies_now, headers=headers)
     return redirect('/main')
 
 def add_equip_of_org(request, org_id):
@@ -126,7 +132,8 @@ def add_equip_of_org(request, org_id):
     post_data = dict(serial = body['serial'], organization = org_id)
     print('Запрос add_equip_of_org')
     print(post_data)
-    response = requests.post("http://127.0.0.1:8000/api/app/equipments/", cookies=cookies_now, headers=headers, data=post_data)
+    response = requests.post("http://127.0.0.1:8000/api/app/equipments/", cookies=cookies_now,
+                             headers=headers, data=post_data)
     if response.status_code == 400:
         return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
     return HttpResponse(status=status.HTTP_200_OK)
@@ -139,7 +146,8 @@ def edit_equip(request, equip_id):
     headers['X-CSRFToken'] = cookies_now['csrftoken']
     body = request.POST
     patch_data = dict(serial=body['serial'])
-    response = requests.patch("http://127.0.0.1:8000/api/app/equipments/" + str(equip_id) + "/", cookies=cookies_now, headers=headers, data=patch_data)
+    response = requests.patch("http://127.0.0.1:8000/api/app/equipments/" + str(equip_id) + "/", cookies=cookies_now,
+                              headers=headers, data=patch_data)
     print("Ты заходи если шо")
     print(response.content)
     if response.status_code == 400:
@@ -155,7 +163,8 @@ def edit_current_user(request):
     print(post_data)
     print("А теперь пользователь")
     current_user = (requests.get("http://127.0.0.1:8000/api/app/current_user/", cookies=cookies_now)).json()
-    response = requests.put("http://127.0.0.1:8000/api/app/users/"+str(current_user['id'])+"/", cookies=cookies_now, headers=headers, data=post_data)
+    response = requests.put("http://127.0.0.1:8000/api/app/users/"+str(current_user['id'])+"/", cookies=cookies_now,
+                            headers=headers, data=post_data)
     if response.status_code == 400:
         return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
     return HttpResponse(status=status.HTTP_200_OK)
@@ -168,15 +177,20 @@ def user_to_admin(request, user_id):
     headers['X-CSRFToken'] = cookies_now['csrftoken']
     patch_data = dict(is_superuser = True)
     current_user = (requests.get("http://127.0.0.1:8000/api/app/current_user/", cookies=cookies_now)).json()
-    response = requests.patch("http://127.0.0.1:8000/api/app/users/" + str(user_id)+"/", cookies=cookies_now, headers=headers, data=patch_data)
+    response = requests.patch("http://127.0.0.1:8000/api/app/users/" + str(user_id)+"/", cookies=cookies_now,
+                              headers=headers, data=patch_data)
     response_cookies = response.cookies
     current_cookies = dict(response_cookies)
     resp = redirect('/main')
     resp.set_cookie('sessionid', current_cookies['sessionid'])
     return resp
-# def edit_equip_of_org(request, equip_uuid):
-#
-#     return redirect('/main')
 
+def delete_org(request, org_id):
+    permission_classes = [IsAdminUser]
+    cookies_now = {'csrftoken': request.COOKIES.get('csrftoken'), 'sessionid': request.COOKIES.get('sessionid')}
+    headers = {}
+    headers['X-CSRFToken'] = cookies_now['csrftoken']
+    response = requests.delete("http://127.0.0.1:8000/api/app/organizations/" + str(org_id), cookies=cookies_now,
+                              headers=headers)
+    return redirect('/main')
 
-#Вьюшки-обработчики экрана админа
